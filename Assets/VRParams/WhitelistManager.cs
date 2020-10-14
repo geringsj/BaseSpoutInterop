3cf1a25047c01e5cc0e6cc3eb40894aaf3eb2e0e
Duc Anh Nguyen <ducanh08@gmx.de>
2020-10-14 13:56:04 +0200

    fixed whitelistmanager

---
+++ Assets/VRParams/WhitelistManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using interop;
+
+public class WhitelistManager : VisParamSender<List<string>>
+{
+    // "SphereRenderer", "RaycastVolumeRenderer", "BoundingBoxRenderer", "VrInteropView3D_2"
+    public List<string> whitelist = new List<string>();
+    Parameter<List<string>> whitelistParam = new Parameter<List<string>>();
+    public WhitelistManager(string name, bool init) : base(name, init) { }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //whitelist = new List<string> {"VrInteropView3D_2" };
+    }
+
+    void Update()
+    {
+
+
+        whitelistParam.name = "Whitelist";
+        whitelistParam.modulFullName = "ModulWhitelist";
+        whitelistParam.param = whitelist;
+        //Debug.Log(whitelist);
+        // send the whitelist
+        Send(whitelistParam);
+    }
+}
